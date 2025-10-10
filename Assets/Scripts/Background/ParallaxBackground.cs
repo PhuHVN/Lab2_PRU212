84a757d0f9a34dadc6b3eb03afa75fe1ce7d25dc
PhuHVN <ngocphu2k4@gmail.com>
2025-10-10 18:15:05 +0700

    demo basegame

--- Assets/Scripts/Background/ParallaxBackground.cs
+++ Assets/Scripts/Background/ParallaxBackground.cs
@@ -2,22 +2,31 @@ using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
-    float backgroundWidth;
+    [SerializeField] private Camera cam;              // Camera (gán MainCamera ho?c Cinemachine camera)
+    [SerializeField] private float parallaxEffect;    // 0.1f - 0.9f (nh? = xa)
+    private float startPosX;
+    private float length;
+
     void Start()
     {
-        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
-        backgroundWidth = sprite.texture.width / sprite.pixelsPerUnit;
+        if (cam == null)
+            cam = Camera.main;
+
+        startPosX = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-
-    void Update()
+    void LateUpdate()
     {
-        float movementX = moveSpeed * Time.deltaTime;
-        transform.position += new Vector3(movementX, 0f, 0f);
-        if (transform.position.x <= -backgroundWidth)
-        {
-            transform.position = new Vector3(transform.position.x + backgroundWidth, transform.position.y, transform.position.z);
-        }
+        float temp = cam.transform.position.x * (1 - parallaxEffect);
+        float dist = cam.transform.position.x * parallaxEffect;
+
+        transform.position = new Vector3(startPosX + dist, transform.position.y, transform.position.z);
+
+        // L?p background n?u camera di chuy?n quá xa
+        if (temp > startPosX + length)
+            startPosX += length;
+        else if (temp < startPosX - length)
+            startPosX -= length;
     }
 }
